73b12019b8102ad4c05fcbb3ef46dac588053709
daoqing99 <daoqing99@163.com>
2018-01-23 10:17:36 +0800

    first commit

---
+++ js/cashier-system.js
@@ -0,0 +1,20 @@
+var cashierSystemContainer = new Vue({
+    el: "#contaniner",
+    data: {
+    	// p1 不是套餐界面
+        p1Show: true,
+        p1ShowChild: true,
+        p2Show: false,
+
+        // p2套餐界面
+        // p1Show: false,
+        // p1ShowChild: true,
+        // p2Show: true,
+
+        // 收银，支付方式界面
+        // p1Show: true,
+        // p1ShowChild: false,
+        // p2Show: false,
+
+    }
+})
